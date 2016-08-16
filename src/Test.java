d6a1ab5e6170a5c3ad7fead37bbc5a8d7217144c
FranDepascuali <francisco.depascuali@wolox.com.ar>
2016-08-16 20:29:01 -0300

    Adding formatter interface

--- src/Test.java
+++ src/Test.java
@@ -13,40 +13,32 @@ public class Test {
   private static final int CONVERGENCE_RADIUS = 10;
 
   public static void main(String[] args) {
-    // Create board
-    Board board = new Board(BOARD_SIZE, CELL_SIZE, CONVERGENCE_RADIUS);
-    // Add random particles to the board
-    for (int i = 0; i < PARTICLE_COUNT; i++) {
-      board.addRandomParticle(i);
-    }
 
-    board.updateCells();
-
-    List<String> sb = new LinkedList<>();
-    sb.add(String.valueOf(PARTICLE_COUNT));
-    sb.add("This is a comment");
-
-    for (int i = 0; i < board.getCellCount(); i++) {
-      for (int j = 0; j < board.getCellCount(); j++) {
-        //System.out.println();
-        //System.out.println("New position: " + i + ", " + j);
-        for (Particle particle: board.at(i, j)) {
-          //System.out.println("Particle " + particle.index + ": " + particle.x + ", " + particle.y);
-          //System.out.print(particle.index);
-          sb.add(particle.id + "\t" + particle.x + "\t" + particle.y);
-          for (Particle neighbourParticle: particle.getNeighbours()) {
-            //System.out.print(" " + neighbourParticle.index);
-          }
-          //System.out.println();
-        }
-      }
-    }
-
-    Path file = Paths.get("output.xyz");
-    try {
-      Files.write(file, sb, Charset.forName("UTF-8"));
-    } catch (IOException ioException) {
-      ioException.printStackTrace();
-    }
+    XYZFormatter formatter = new XYZFormatter();
+    Board board = initializeBoard();
+    
+    List<String> lines = formatter.fformat(board, PARTICLE_COUNT);
+    writeTo("output.xyz", lines);
+  }
+  
+  private static Board initializeBoard() {
+	    // Create board
+	    Board board = new Board(BOARD_SIZE, CELL_SIZE, CONVERGENCE_RADIUS);
+	    // Add random particles to the board
+	    for (int i = 0; i < PARTICLE_COUNT; i++) {
+	      board.addRandomParticle(i);
+	    }
+	    
+	    board.updateCells();
+	    return board;
+  }
+  
+  private static void writeTo(String fileName, List<String> lines) {
+	  Path path = Paths.get(fileName);
+	  try {
+		  Files.write(path, lines, Charset.forName("UTF-8"));
+	  } catch (IOException ioException) {
+		  ioException.printStackTrace();
+	  }
   }
 }
