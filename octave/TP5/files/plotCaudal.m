82945951b441f9a3864c58e4c0e424dc8e93f89e
Francisco Depascuali <FranDepascuali@Franciscos-MacBook-Pro.local>
2016-10-24 20:31:45 -0300

    Correcting statistics plots

--- octave/TP5/files/plotCaudal.m
+++ octave/TP5/files/plotCaudal.m
@@ -11,16 +11,23 @@ endfor
 t = 1:length(h);
 t = t .* 0.1;
 
-plot(t,h)
+m = mean(h);
+M = zeros(length(t), 1);
+for i = 1: length(t)
+  M(i) = m;
+endfor
 
+plot(t, h, t, M, 'r')
 title ("Caudal");
 
 ylabel ("Q (1/s)");
 xlabel ("Tiempo (s)");
 
-fontsize=20;
+fontsize=16;
 set([gca; findall(gca, 'Type','text')], 'FontSize', fontsize);
 set([gca; findall(gca, 'Type','line')], 'linewidth', 3);
-hx=legend('example');set(hx, "fontsize", fontsize)
+
+
+
 
 endfunction
--- octave/TP5/files/plotCinetica.m
+++ octave/TP5/files/plotCinetica.m
@@ -1,16 +1,19 @@
 function result = plotCinetica(file_name)
 
 x = load(file_name);
-plot(x)
+
+t = 1:length(x);
+t = t .* 0.1;
+
+plot(t,x)
 
 title ("Energía Cinética");
 
 ylabel ("K (J)");
 xlabel ("Tiempo (s)");
 
-fontsize=20;
+fontsize=16;
 set([gca; findall(gca, 'Type','text')], 'FontSize', fontsize);
 set([gca; findall(gca, 'Type','line')], 'linewidth', 3);
-hx=legend('example');set(hx, "fontsize", fontsize)
 
 endfunction
